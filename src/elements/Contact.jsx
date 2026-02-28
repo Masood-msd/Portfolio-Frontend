5c7ebb7482c95270f1824aff2a7eced8a7b5ee92
Masood-msd <masoodsd2004@gmail.com>
2026-02-28 14:51:15 +0530

    contact button changes

--- src/elements/Contact.jsx
+++ src/elements/Contact.jsx
@@ -112,7 +112,7 @@ useEffect(() => {
               rows="5"
             ></textarea>
 
-            <button type="button">Send Message</button>
+            <button type="submit">Send Message</button>
           </form>
         </div>
       </section>
