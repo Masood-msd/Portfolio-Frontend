7379856b1ae9582748fa5ca8f64da828ef0fe69e
Masood-msd <masoodsd2004@gmail.com>
2026-02-28 12:48:27 +0530

    changes in about page

--- src/elements/About.jsx
+++ src/elements/About.jsx
@@ -1,18 +1,11 @@
 import '../styles/about.css'
 import { useAuth } from '../store/auth'
-//import { useEffect, useState } from 'react'
+
 
 export default function About() {
 
-    // const [details, setDetails] = useState({ username: ""})
-     const {user} = useAuth()
+     const {user, API} = useAuth()
 
-     //  useEffect(()=>{
-     //      if(user){
-     //           // eslint-disable-next-line react-hooks/set-state-in-effect
-     //           setDetails({username: user.username})
-     //      }
-     // },[user])
      return (
           <>
             <main className="about-page">
@@ -21,7 +14,7 @@ export default function About() {
                          <h2>Welcome, {user ? `${user.username} to my website `: `to our website`}</h2> 
                          <h3>Let's Connect</h3>
                          <p>I'm a fresher full-stack developer, actively looking for opportunities to learn, grow, and contribute to real-world projects. Feel free to reach out for collaboration, feedback, or job opportunities.</p>
-                         <a href="/Contact" className='about-btn'>Contact Me</a>
+                         <a href={`${API}/Contacts`} className='about-btn'>Contact Me</a>
                     </div>
                     <div className="about-img">
                          <img src="/about.png" alt="About us image" />
